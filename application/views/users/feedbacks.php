c2931a5c26f19b7dea3e646c63b9059a7b02f86d
john <olatayoefficient@gmail.com>
2021-02-15 10:54:21 +0530

    update

---
+++ application/views/users/feedbacks.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/feedbacks.css'); ?>">
+<div class="mt-3 mr-3 ml-3">
+	<div class="bg-light table-responsive">
+		<div class="pt-3 pb-3 pl-3">
+			<a href="<?php echo base_url("user/export_feedbacks") ?>" class="btn text-light" style="background: #141E30;border-radius: 0">
+				<i class="fas fa-file-csv mr-2"></i>CSV Download
+			</a>
+		</div>
+		<table class="table">
+			<tr class="text-center text-light" style="background: #141E30">
+				<th>ID</th>
+				<th>Name</th>
+				<th>E-mail</th>
+				<th>Message</th>
+			</tr>
+			<?php if($feedbacks->num_rows() <= 0): ?>
+				<tr>
+					<td colspan="4" class="text-dark text-center font-weight-bolder">No data</td>
+				</tr>
+			<?php endif; ?>
+			<?php if($feedbacks->num_rows() > 0): ?>
+				<?php foreach($feedbacks->result_array() as $msg): ?>
+					<tr class="text-center">
+						<td><?php echo $msg['id'] ?></td>
+						<td><?php echo $msg['name'] ?></td>
+						<td><?php echo $msg['user_mail'] ?></td>
+						<td><?php echo $msg['bdy'] ?></td>
+					</tr>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</table>
+	</div>
+</div>
---
+++ application/views/users/websites.php
@@ -0,0 +1,195 @@
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/websites.css'); ?>">
+<div class="mr-3 ml-3 mt-3 bg-light" id="content">
+    <div class="modal add_web_modal">
+        <div class="modal-dialog modal-dialog-top">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <form method="post" action="<?php echo base_url("user/add_website") ?>" class="add_web_modal_form">
+                        <input type="hidden" class="csrf_token" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
+                        <div class="form-group">
+                            <label>Website Name</label>
+                            <input type="text" name="web_name" class="web_name form-control" placeholder="Name of the webisite" required>
+                        </div>
+                        <div class="form-group">
+                            <label class="mb-0">Website Link</label>
+                            <div class="text-danger font-weight-bolder mt-0 web_link_err"></div>
+                            <input type="url" name="web_link" class="web_link form-control" placeholder="e.g https://domainname.com" required>
+                        </div>
+                        <div class="modal_btn_actions d-flex justify-content-between">
+                            <button type="button" class="btn btn-secondary closewebmodal_btn">Close</button>
+                            <button type="submit" class="btn add_web_modal_btn text-light" style="background-color:#141E30;">Add</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <form action="<?php echo base_url('user/addwebsites'); ?>" method="post" class="addweb_form">
+        <h4 class="text-center mt-3 mb-0">WEBSITES</h4>
+        <hr class="mt-2 h_hr">
+        <input type="hidden" class="csrf_token" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
+        <div class="text-right">
+            <button type="button" class="text-light btn addwebmodal_btn" style="background:#141E30">
+                <i class="fas fa-plus-circle mr-2"></i>Add
+            </button>
+        </div>
+        <div class="text-danger text-left instruction_div">
+            <div>You can create a maximum of 10 Webistes.</div>
+            <div>For more quota, contact us at <a href="mailto:hr@nktech.in">nktech.in</a> for your desired package.</div>
+            <div>You can't edit, rename or change any of this information after.</div>
+        </div>
+        <hr>
+        <div class="text-center countwebs_div">
+            Created <span class="countwebs">0</span> website(s) out of 10
+        </div>
+        <div class="web_info_div" id="web_info_div">
+        </div>
+
+        <div class="btngrp text-right mt-5" style="display:none">
+            <input type="hidden" class="num_inc" value="1">
+            <button class="btn text-light add_allwebbtn" type="submit" style="background:#141E30">Create</button>
+        </div>
+    </form>
+</div>
+
+
+
+<script>
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $(document).on('click', 'button.addwebmodal_btn', function(e) {
+            e.preventDefault();
+            var num = $('.num_inc').val();
+
+            if (parseInt(num) == 11) {
+                $(".add_web_modal_btn").hide();
+                $('.web_name,.web_link').attr("readonly", "true");
+                $('.add_web_modal').modal("hide");
+            } else {
+                $('.add_web_modal').modal("show");
+            }
+        });
+
+        $(document).on('click', 'button.closewebmodal_btn', function(e) {
+            e.preventDefault();
+            $('.add_web_modal').modal("hide");
+        });
+
+        $(document).on('click', 'button.add_web_modal_btn', function(e) {
+            e.preventDefault();
+            var csrfName = $('.csrf_token').attr('name');
+            var csrfHash = $('.csrf_token').val();
+            var web_name = $('.web_name').val();
+            var web_link = $('.web_link').val();
+            var num = $('.num_inc').val();
+
+            if (web_name == "" || web_name == null) {
+                $('.web_name').css('border', '2px solid red');
+                return false;
+            } else {
+                $('.web_name').css('border', '1px solid #ced4da');
+            }
+            if (web_link == "" || web_link == null) {
+                $(".web_link").css('border', '2px solid red');
+                return false;
+            }
+
+            var patt = new RegExp('^(https?:\\/\\/)?' + // protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))' + // ip (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + //port
+                '(\\?[;&amp;a-z\\d%_.~+=-]*)?' + // query string
+                '(\\#[-a-z\\d_]*)?$', 'i');
+            var res = patt.test(web_link);
+            if (res == true) {
+                $(".web_link_err").fadeOut();
+                $('.web_link').css('border', '1px solid #ced4da');
+            } else if (res == false) {
+                $(".web_link").css('border', '2px solid red');
+                $(".web_link_err").html("Invalid WEB URL").fadeIn();
+                return false;
+            }
+
+            $(".web_info_div").append('<div class="d-flex flex-direction-row col-md-12 web_wrapper" id="' + num + '" webname="' + web_name + '" weblink="' + web_link + '"><div class="form-group" style="margin:auto 0"><span class="web_num"><i class="fas fa-circle"></i></span></div><div class="form-group col"><label class="webname_label" id="webname_label">' + web_name + '</label><input type="url" name="weblink_input" class="form-control weblink_input" id="weblink_input" value="' + web_link + '" readonly required></div><div class="form-group" style="margin:auto 0"><i class="fas fa-times remove_web_i text-danger" web_id="' + num + '" web_name="' + web_name + '" web_link="' + web_link + '"></i></div></div>');
+
+            $('.add_web_modal').modal("hide");
+            $('.web_name').val("");
+            $('.web_link').val("");
+
+            var new_num = parseInt(num) + 1;
+            $('.num_inc').val(new_num);
+
+            var countwebs = $('.web_wrapper').length;
+            $(".countwebs").html(countwebs);
+            $(".countwebs_div").fadeIn();
+
+            $(".btngrp").fadeIn();
+        });
+
+        $(document).on('click', 'i.remove_web_i', function() {
+            var csrfName = $('.csrf_token').attr('name');
+            var csrfHash = $('.csrf_token').val();
+            var web_name = $(this).attr("web_name");
+            var web_link = $(this).attr("web_link");
+
+            console.log(web_link);
+            console.log(web_name);
+
+            var num = $('.num_inc').val();
+            var web_id = $(this).attr("web_id");
+
+            $("div#" + web_id).remove();
+
+            var countwebs = $('.web_wrapper').length;
+            $(".countwebs").html(countwebs);
+            if (parseInt(countwebs) == 0) {
+                $(".btngrp").fadeOut();
+            } else {
+                $(".btngrp").fadeIn();
+            }
+
+            // var new_num = parseInt(num) - 1;
+            // $('.num_inc').val(new_num);
+        });
+
+        $(document).on('click', '.add_allwebbtn', function(e) {
+            e.preventDefault();
+            var csrfName = $('.csrf_token').attr('name');
+            var csrfHash = $('.csrf_token').val();
+
+            var webname_arr = [];
+            var weblink_arr = [];
+            $(".web_wrapper").each(function() {
+                var webname = $(this).attr("webname");
+                var weblink = $(this).attr("weblink");
+
+                webname_arr.push(webname);
+                weblink_arr.push(weblink);
+            });
+
+            $.ajax({
+                method: "post",
+                url: "<?php echo base_url("user/addwebsites") ?>",
+                data: {
+                    webname_arr: webname_arr,
+                    weblink_arr: weblink_arr,
+                    [csrfName]: csrfHash
+                },
+                dataType: "json",
+                success: function(data) {
+                    $('.csrf_token').val(data.token);
+                    if (data.status == false) {
+                        window.location.assign(data.msg);
+                    } else if (data.status == "error") {
+                        $(".ajax_succ_div").fadeOut();
+                        $('.ajax_res_err').html(data.msg);
+                        $('.ajax_err_div').fadeIn();
+                    } else if (data.status == true) {
+                        window.location.assign(data.msg);
+                    }
+                }
+            });
+        });
+    });
+</script>

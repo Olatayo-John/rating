d76af5140f49e62390456d5ea353e61b2498b9b1
john <olatayoefficient@gmail.com>
2021-11-09 16:38:40 +0530

    new file:   application/views/templates/fof.php

---
+++ application/views/templates/fof.php
@@ -0,0 +1,28 @@
+<div id="container">
+    <h1>404 Page Not Found</h1>
+    <p>The page you requested was not found.</p>
+</div>
+
+
+<style>
+    #container {
+        margin-top: 95px;
+        border: 1px solid #fff;
+        box-shadow: 0 0 8px #fff;
+    }
+
+    h1 {
+        color: #fff;
+        background-color: transparent;
+        border-bottom: 1px solid #fff;
+        font-size: 19px;
+        font-weight: normal;
+        margin: 0 0 14px 0;
+        padding: 14px 15px 10px 15px;
+    }
+
+    p {
+        color: #fff;
+        margin: 12px 15px 12px 15px;
+    }
+</style>

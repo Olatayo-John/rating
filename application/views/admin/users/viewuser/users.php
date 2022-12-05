cd820f65d8fc0eb739b042b1662f67ed954d24eb
john <olatayojohn10@gmail.com>
2022-12-05 17:12:49 +0530

    cmpy-users

---
+++ application/views/admin/users/viewuser/users.php
@@ -0,0 +1,24 @@
+<div class="row col-md-12 mb-3 p-0">
+    <div class="col-lg-4 col-xs-12 col-md-12 total-column">
+        <div class="panel_s">
+            <div class="panel-body">
+                <h3 class="_total userst"></h3>
+                <span>Users</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<table id="adminuserstable" data-search="true" data-show-export="true" data-buttons-prefix="btn-md btn" data-buttons-align="right" data-pagination="true">
+    <thead class="text-light" style="background:#294a63">
+        <tr>
+            <th data-field="uname" data-sortable="true">Username</th>
+            <th data-field="sub" data-sortable="true">Subscription</th>
+            <th data-field="active" data-sortable="true">Account Status</th>
+        </tr>
+    </thead>
+</table>
+
+<script>
+
+</script>
